0ad60861c4944bcf85b108816d7b61fb6b5547ff
Flavio <flavio@gmail.com>
2019-11-30 12:00:41 +0100

    square calculator

---
+++ index.js
@@ -0,0 +1,12 @@
+function squareArea(side) {
+    return side * side;
+} 
+
+function squareArea(radius){
+    return radius * radius * Math.PI;
+}
+
+module.exports = {
+    squareArea,
+    squareArea,
+};
